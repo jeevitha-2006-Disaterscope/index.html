4cc172f81990ff4b1a732dd32f6d608881ea459e
jeevitha-2006-Disaterscope <jeevithasaravanan2006@gmail.com>
2025-06-11 18:41:28 +0530

    Create features.js

---
+++ features.js
@@ -0,0 +1,13 @@
+export default function Features() {
+  return (
+    <div>
+      <h1>Features</h1>
+      <ul>
+        <li>Live Disaster Tracking</li>
+        <li>AI Prediction Models</li>
+        <li>User Alerts & Notifications</li>
+        <li>Interactive Maps</li>
+      </ul>
+    </div>
+  );
+}
