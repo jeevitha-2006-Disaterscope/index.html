cc1fa3cb9ac333dc9d9f2290f6ee55bebd85725e
jeevitha-2006-Disaterscope <jeevithasaravanan2006@gmail.com>
2025-06-11 18:40:54 +0530

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Email: support@disasterscope.com</p>
+      <p>Phone: +91 98765 43210</p>
+    </div>
+  );
+}
