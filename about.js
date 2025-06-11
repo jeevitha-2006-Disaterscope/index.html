3ac1832eac018b98f8aa091b004f5d322904091c
jeevitha-2006-Disaterscope <jeevithasaravanan2006@gmail.com>
2025-06-11 18:40:11 +0530

    Create about.js

---
+++ about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About DisasterScope</h1>
+      <p>This app helps predict and alert about natural disasters using AI and real-time data.</p>
+    </div>
+  );
+}
